2ef16a2540cf44eb659208f5d92e942073d07bfa
Atikah <59849416+atikah2301@users.noreply.github.com>
2020-10-06 10:17:13 +0100

    Update euclid's algorithm .py

--- euclid's algorithm .py
+++ euclid's algorithm .py
@@ -1,8 +1,10 @@
 # from math import *
 # math class comes with a method for gcd:
+# rudimentary gcd calculator, with no checks on user input
+# uses mod operator instead of mathematical notation
 
 def gcd(a, b):
-  while b!=0:
+  while b>0:
     a, b = b, a % b
     print(a, b)
   return a
