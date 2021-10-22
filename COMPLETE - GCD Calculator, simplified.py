1363ff26413772338b89cb7e65e16abfdf5c599a
Atikah <atikah.hussain@hotmail.co.uk>
2021-10-22 15:18:12 +0100

    Add while loop for continuous input and output

--- COMPLETE - GCD Calculator, simplified.py
+++ COMPLETE - GCD Calculator, simplified.py
@@ -6,3 +6,12 @@ def gcd(a, b):
     a, b = b, a % b
     #print("=gcd("+str(a)+","+str(b)+")")
   return a
+
+while True:
+  input_ = input("a,b = ")
+  if input_:
+    a, b = tuple(input_.split(","))  
+    a, b = int(a), int(b)
+    print(f"gcd({a},{b})={gcd(a, b)}")
+  else:
+    break
