a10d37fdc4170961ee6f846ef0b9ccebeb56ef8c
Atikah <atikah.hussain@hotmail.co.uk>
2021-11-22 19:07:17 +0000

    Create variables for the english alphabet and its standard letter frequencies

--- COMPLETE - Vigenere Cipher Decoder.py
+++ COMPLETE - Vigenere Cipher Decoder.py
@@ -18,6 +18,22 @@ from collections import Counter
 from tabulate import tabulate
 from math import ceil as round_up
 
+# Used to retrieve caesar shift lengths from keywords
+english_alphabet = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+# Used to calculated expected frequencies for Chi Squared stat
+standard_letter_frequencies = [
+    0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
+    0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
+    0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+    0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
+
+# Used to map letters to their frequencies
+english_letters_and_frequencies = list(zip(english_alphabet, standard_letter_frequencies))
+
+
 def gcd(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
