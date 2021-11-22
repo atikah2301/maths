9d05f92d1f667bf8cdb3a00bf98ce180b9d0d0ef
Atikah <atikah.hussain@hotmail.co.uk>
2021-11-22 19:04:27 +0000

    Find and tabulate the frequencies of the ngrams

--- COMPLETE - Vigenere Cipher Decoder.py
+++ COMPLETE - Vigenere Cipher Decoder.py
@@ -31,3 +31,30 @@ def get_ngrams(text: int, n=1) -> list[str]:
         yield text[i: i + n]
 
 
+def find_positions(ciphertext: str, phrase: str) -> list[int]:
+    # Step (2)
+    all_indices = []
+    index = ciphertext.find(phrase, 0, len(ciphertext) - 1)
+    while index != -1:
+        all_indices.append(index)
+        index = ciphertext.find(phrase, index + len(phrase), len(ciphertext) - 1)
+    return all_indices
+
+
+def tabulate_frequencies(counter: Counter, title: str, threshold=1) -> None:
+    # Step (6) is done intuitively by making a guess based on the tabulated results
+    # Only tabulate values with a frequency above a given threshold
+    counter_sorted_by_value = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+    significant_results = []
+    for key, value in counter_sorted_by_value:
+        if value > threshold:
+            significant_results.append((key, value))
+
+    significant_values = [key for key, value in significant_results]
+    significant_values.insert(0, title)
+    significant_frequencies = [value for key, value in significant_results]
+    significant_frequencies.insert(0, "Frequency")
+    table = tabulate([significant_values, significant_frequencies])
+    print(table)
+
+
