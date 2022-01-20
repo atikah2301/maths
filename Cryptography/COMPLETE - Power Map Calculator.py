60057ce319fd452dcf3efc2ad110ce5d7fbcad0c
Atikah <atikah.hussain@hotmail.co.uk>
2022-01-20 03:22:27 +0000

    Complete step 1

--- Cryptography/COMPLETE - Power Map Calculator.py
+++ Cryptography/COMPLETE - Power Map Calculator.py
@@ -9,3 +9,23 @@
 
 from math import log
 from tabulate import tabulate
+
+def get_sum_of_powers_of_2(e: int) -> list[int]:
+    """
+    Returns a list of integers which are the powers for each term in a sum of powers of 2 that equal e.
+    E.g. for e=321, return [8,6,0] since e=2^8+2^6+2^0
+
+    Step 1 of evaluating a power map.
+
+    Calculate log2(e), round it down, add to a result list
+    If log2(e) is a whole number, then stop, and return the result
+    Otherwise let q be log2(e) rounded down and calculate e - 2^q
+    Let this be the new e, repeat"""
+    result = []
+    f = e # we don't want to reassign e to anything else
+    while True:
+        q = log(f,2)
+        result.append(int(q))
+        if q == int(q):
+            return result
+        f = f - 2**int(q)
