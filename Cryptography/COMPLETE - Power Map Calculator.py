78435260cc616fc595290095d1e922be6859a54d
Atikah <atikah.hussain@hotmail.co.uk>
2022-01-20 03:21:55 +0000

    Plan the implementation

--- Cryptography/COMPLETE - Power Map Calculator.py
+++ Cryptography/COMPLETE - Power Map Calculator.py
@@ -1,3 +1,7 @@
 # Calculates T(x), which is x to the power of some e, all mod n
 # e.g. if e=2 and n=3 then T(12) = 12^2 mod 3 = 24 mod 3 = 0
 
+# Step 1: Find e as the unique minimal sum of powers of 2
+# Step 2: Calculate 2^k for all k up to the maximum power needed from step 1
+# Step 3: Calculate x^2^k for all k.. iteratively, by squaring the previous value and reducing mod n
+# Step 4: Multiply together the x^2^k values needed to get x^e and reduce mod n
