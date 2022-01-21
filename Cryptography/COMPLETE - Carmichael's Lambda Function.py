fd7bec63646e7c49c2af9e3b8a45c3326777cf39
Atikah <atikah.hussain@hotmail.co.uk>
2022-01-21 14:04:31 +0000

    Create COMPLETE - Carmichael's Lambda Function.py

---
+++ Cryptography/COMPLETE - Carmichael's Lambda Function.py
@@ -0,0 +1,3 @@
+# Carmichael's Lambda Function is calculates the SMALLEST POSITIVE m
+# for n such that a^m is congruent to 1 mod n
+# for the group of all unit elements in the ring of all congruence classes mod n
