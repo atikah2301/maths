00c86876c5ed03c3a9c82f76bf10fd1ceffa0ce2
Atikah <atikah.hussain@hotmail.co.uk>
2022-01-21 14:04:53 +0000

    Plan the implementation of the lambda function

--- Cryptography/COMPLETE - Carmichael's Lambda Function.py
+++ Cryptography/COMPLETE - Carmichael's Lambda Function.py
@@ -1,3 +1,12 @@
 # Carmichael's Lambda Function is calculates the SMALLEST POSITIVE m
 # for n such that a^m is congruent to 1 mod n
 # for the group of all unit elements in the ring of all congruence classes mod n
+
+from math import lcm
+
+def carmichael(n: list) -> int:
+    """
+    n: list of tuples of integers, representing the prime factorisation of n
+    so for 819 we'd have n=[(3,2),(7,1),(13,1)]
+    where the first number in the tuple is the base, and second is the exponent
+    """
